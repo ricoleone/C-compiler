57b0cbd60eb7d3f6e784ed7b92a62cffe694d202
ricoleone <richard.a.leone@gmail.com>
2023-09-24 05:20:05 -0400

    Added helper files

---
+++ helpers/buffer.h
@@ -0,0 +1,32 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define BUFFER_REALLOC_AMOUNT 2000 //refactor to page size
+struct buffer
+{
+    char* data;
+    // Read index
+    int rindex;
+    int len;
+    int msize;
+};
+
+struct buffer* buffer_create();
+
+char buffer_read(struct buffer* buffer);
+char buffer_peek(struct buffer* buffer);
+
+void buffer_extend(struct buffer* buffer, size_t size);
+
+//when printing to the buffer, it will resize if the current length is insufficient
+void buffer_printf(struct buffer* buffer, const char* fmt, ...);
+void buffer_printf_no_terminator(struct buffer* buffer, const char* fmt, ...);
+void buffer_write(struct buffer* buffer, char c);
+void* buffer_ptr(struct buffer* buffer);
+void buffer_free(struct buffer* buffer);
+
+
+#endif
