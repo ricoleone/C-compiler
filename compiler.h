ff73e56e459770fc9e0e615b643a4936a961fea0
ricoleone <richard.a.leone@gmail.com>
2023-09-29 12:35:49 -0400

    Implement Lexer Framework

--- compiler.h
+++ compiler.h
@@ -2,7 +2,91 @@
 #define LE1_COPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
+#define NUMERIC_CASE \
+    case '0':        \
+    case '1':        \
+    case '2':        \
+    case '3':        \
+    case '4':        \
+    case '5':        \
+    case '6':        \
+    case '7':        \
+    case '8':       \
+    case '9'
+
+
+struct pos
+{
+    int line;
+    int col;
+    const char *filename;
+};
+
+
+enum
+{
+    LEXICAL_ANALYSIS_ALL_OK,
+    LEXICAL_ANALYSIS_INPUT_ERROR
+};
+
+enum
+{
+    TOKEN_IDENTIFIER,
+    TOKEN_KEYWORD,
+    TOKEN_OPERATOR,
+    TOKEN_SYMBOL,
+    TOKEN_NUMBER,
+    TOKEN_STING,
+    TOKEN_COMMENT,
+    TOKEN_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+    struct pos pos;
+    union
+    {
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void *any;
+    };
+    //True is there is whitespace between tokens
+    bool whitespace;
+    const char *between_brakets;
+};
+
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process *process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process *process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process *process, char c);
+
+struct lex_process_functions
+{
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+struct lex_process
+{
+    struct pos pos;
+    struct vector *token_vec;
+    struct compile_process *compiler;
+
+    // how many nested brakets
+    int current_expresion_count;
+    struct buffer *parentheses_buffer;
+    struct lex_process_functions *function;
+    // holds private data
+    void *private;    
+};
 enum
 {
     COMPILER_FILE_COMPILED_OK,
@@ -12,7 +96,7 @@ enum
 struct compile_process
 {
     int flags; //Compilation flags
-    
+    struct pos pos;
     struct compile_process_input_file
     {
         FILE *fp;
@@ -23,5 +107,19 @@ struct compile_process
 };
 
 int compile_file(const char *filename, const char *out_file, int flags);
-struct compile_process *compile_process_create(const char *filename, const char *filename_out, int flags);
+struct compile_process* compile_process_create(const char *filename, const char *filename_out, int flags);
+
+char compile_process_next_char(struct lex_process *lex_process);
+char compile_process_peek_char(struct lex_process *lex_process);
+void compile_process_push_char(struct lex_process *lex_process, char c);
+
+void compiler_error(struct compile_process *compiler, const char *msg, ...);
+void compiler_warning(struct compile_process *compiler, const char *msg, ...);
+
+struct lex_process *lex_process_create(struct compile_process *compiler, struct lex_process_functions *functions, void *private);
+void lex_process_free(struct lex_process *process);
+void *lex_process_private(struct lex_process *process);
+struct vector *lex_process_tokens(struct lex_process *process);
+
+int lex(struct lex_process *process);
 #endif
