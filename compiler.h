d006d38a401d188fd87c9a726304cb4e400dcdfc
ricoleone <richard.a.leone@gmail.com>
2023-10-04 07:20:24 -0400

    SCRUM-24 Add case to create tokens outside the input file

--- compiler.h
+++ compiler.h
@@ -154,5 +154,10 @@ struct vector *lex_process_tokens(struct lex_process *process);
 
 int lex(struct lex_process *process);
 
+/*
+ * Builds tokens for input strings      
+*/
+struct lex_process *tokens_build_for_string(struct compile_process *compiler, const char *str);
+
 bool token_is_keyword(struct token *token, const char *value);
 #endif
--- lexer.c
+++ lexer.c
@@ -25,7 +25,7 @@ static char peekc()
 static char nextc()
 {
     char c = lex_process->function->next_char(lex_process);
-    if(lex_is_in_expression())
+    if (lex_is_in_expression())
     {
         buffer_write(lex_process->parentheses_buffer, c);
     }
@@ -59,7 +59,7 @@ struct token *token_create(struct token *_token)
 {
     memcpy(&tmp_token, _token, sizeof(struct token));
     tmp_token.pos = lex_file_position();
-    if(lex_is_in_expression())
+    if (lex_is_in_expression())
     {
         tmp_token.between_brakets = buffer_ptr(lex_process->parentheses_buffer);
     }
@@ -449,7 +449,7 @@ const char *read_hex_number_str()
 {
     struct buffer *buffer = buffer_create();
     char c = peekc();
-    //TO DO Needs to throw an error if there is an invalid character in the hex number
+    // TO DO Needs to throw an error if there is an invalid character in the hex number
     LEX_GETC_IF(buffer, c, is_hex_char(c));
     buffer_write(buffer, 0x00);
     return buffer_ptr(buffer);
@@ -593,4 +593,43 @@ int lex(struct lex_process *process)
     }
 
     return LEXICAL_ANALYSIS_ALL_OK;
-};
+}
+
+char lexer_string_buffer_next_char(struct lex_process *process)
+{
+    struct buffer *buf = lex_process_private(process);
+    return buffer_read(buf);
+}
+
+char lexer_string_buffer_peek_char(struct lex_process *process)
+{
+    struct buffer *buf = lex_process_private(process);
+    return buffer_peek(buf);
+}
+
+void lexer_string_buffer_push_char(struct lex_process *process, char c)
+{
+    struct buffer *buf = lex_process_private(process);
+    return buffer_write(buf, c);
+}
+
+struct lex_process_functions lexer_string_buffer_functions = {
+    .next_char = lexer_string_buffer_next_char,
+    .peek_char = lexer_string_buffer_peek_char,
+    .push_char = lexer_string_buffer_push_char};
+
+struct lex_process *tokens_build_for_string(struct compile_process *compiler, const char *str)
+{
+    struct buffer *buffer = buffer_create();
+    buffer_printf(buffer, str);
+    struct lex_process *lex_process = lex_process_create(compiler, &lexer_string_buffer_functions, buffer);
+    if(!lex_process)
+    {
+        return NULL;
+    }
+    if(lex(lex_process) != LEXICAL_ANALYSIS_ALL_OK)
+    {
+        return NULL;
+    }
+    return lex_process;
+}
